5dc55950a0ec0ccf63d6f81fb9c3b4c8f6fdd726
iinigmatullin <iinigmatullin@diasoft.ru>
2025-10-26 19:45:57 +0500

    Убрать синхронность в логике отправки курса валют

--- exchangeGenerator/src/main/java/org/company/exchangegenerator/kafka/publisher/RatePublisher.java
+++ exchangeGenerator/src/main/java/org/company/exchangegenerator/kafka/publisher/RatePublisher.java
@@ -3,16 +3,8 @@ package org.company.exchangegenerator.kafka.publisher;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.company.exchangegenerator.dto.CurrencyListDto;
-import org.company.exchangegenerator.exception.ExchangeGeneratorException;
-import org.springframework.http.HttpStatus;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
-
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
-
-import static java.lang.String.format;
 
 @Slf4j
 @Service
@@ -22,15 +14,14 @@ public class RatePublisher {
     private final KafkaTemplate<String, CurrencyListDto> kafkaTemplate;
 
     public void publish(CurrencyListDto updatedCurrencies) {
-        try {
-            kafkaTemplate.send("exchange-topic", updatedCurrencies)
-                    .get(10, TimeUnit.SECONDS);
-        } catch (InterruptedException | ExecutionException | TimeoutException e) {
-            throw new ExchangeGeneratorException(
-                    format("Ошибка отправки уведомления в топик %s", e.getMessage()),
-                    HttpStatus.INTERNAL_SERVER_ERROR
-            );
-        }
-
+        kafkaTemplate.send("exchange-topic", updatedCurrencies)
+                .whenComplete((result, throwable) -> {
+                    if (throwable != null) {
+                        log.error("Ошибка отправки уведомления в топик exchange-topic", throwable);
+                    } else {
+                        log.debug("Сообщение успешно отправлено в топик exchange-topic, offset: {}",
+                                result.getRecordMetadata().offset());
+                    }
+                });
     }
 }
